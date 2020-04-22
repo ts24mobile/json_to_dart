83d777ba1a92b2d1476ac4f1c5d6b50a37f9571f
ts24corp <play@ts24.com.vn>
2020-04-22 08:15:54 +0700

    up

---
+++ example/generate_model_example.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+import "package:path/path.dart" show dirname, join, normalize;
+import '../lib/json_to_dart.dart';
+
+String _scriptPath() {
+  var script = Platform.script.toString();
+  if (script.startsWith("file://")) {
+    script = script.substring(7);
+  } else {
+    final idx = script.indexOf("file:/");
+    script = script.substring(idx + 5);
+  }
+  return script;
+}
+
+main() {
+  final classGenerator = new ModelGenerator('InvoiceStatistic');
+  final currentDirectory = dirname(_scriptPath());
+  final filePath =
+      normalize(join(currentDirectory.substring(1), 'sample.json'));
+  final jsonRawData = new File(filePath).readAsStringSync();
+  DartCode dartCode = classGenerator.generateDartClasses(jsonRawData);
+  print(dartCode.code);
+}
